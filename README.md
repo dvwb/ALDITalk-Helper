2f266e6e51a9230cf7f9d416db18656142aa1fec
dvwb <github@dvwb.cc>
2026-09-08 23:29:03 +0200

    [+] imports [/] readme changes

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ALDITalk-Helper
 Ein Bot der es automatisiert Datenvolumen bei ALDITalk nachzubuchen anstatt es manuell per App zu machen.
+
+# Requirements:
+`pip install json5`\
+`pip install curl_cffi --upgrade`
--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+import os
+import time
+import json5
+import sys
+import io
+import re
+import random
+import curl_cffi
+
+# CONFIG LOADER
+with open("config.json5", "r") as f:
+    config = json5.load(f)
+
 RUFNUMMER = config["RUFNUMMER"]
 PASSWORT = config["PASSWORT"]
+
