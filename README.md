67e756d991308d4341dc981498c53f77a700fe1f
dvwb <github@dvwb.cc>
2026-09-09 00:31:31 +0200

    [+] playwright basics [+] added ruff for formatting [/] README.md changes

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 Ein Bot der es automatisiert Datenvolumen bei ALDITalk nachzubuchen anstatt es manuell per App zu machen.
 
 # Requirements:
+`requests`\
 `json5`\
 `curl_cffi`\
-`playwright`
+`playwright`\
+`ruff`
+
+# Tutorial:
+
+ 1. `pip install -r requirements.txt`
+ 2. `config.json5` konfigurieren
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import time
+import traceback
+
 import json5
 import sys
 import io
@@ -7,6 +9,7 @@ import re
 import random
 import curl_cffi
 from playwright.sync_api import sync_playwright
+from playwright.sync_api import Error as PlaywrightError
 
 # CONFIGLOADER
 with open("config.json5", "r") as f:
@@ -21,14 +24,41 @@ UEBERSICHT_URL = "https://www.alditalk-kundenportal.de/portal/auth/buchungsueber
 DASHBOARD_URL = "https://www.alditalk-kundenportal.de/portal/auth/uebersicht/"
 
 # BROWSER & HTML ID's / Classes
-UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/152.0.0.0 Safari/537.36" # CHROME 152
+UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/152.0.0.0 Safari/537.36"  # CHROME 152
 HEADLESS = config["HEADLESS"]
 
 # SEITE: LOGIN_URL
 RUFNUMMER_INPUT = "input-5"
 PASSWORT_INPUT = "input-6"
-LOGIN_BUTTONCLASS = "button button--solid button--medium button--color-default button--has-label"
+LOGIN_BUTTONCLASS = (
+    "button button--solid button--medium button--color-default button--has-label"
+)
 
 # SEITE: DASHBOARD_URL
 TOPUP_BUTTONCLASS = "button button--solid button--medium button--color-default button--has-label button--circle"
 
+with sync_playwright() as p:
+    try:
+        browser = p.chromium.launch(headless=HEADLESS)
+        page = browser.new_page()
+
+        # LOGIN-URL ÖFFNEN
+        page.goto(LOGIN_URL, wait_until="domcontentloaded")
+
+        # RUFNUMMER EINGEBEN
+        page.click(RUFNUMMER_INPUT)
+        page.fill(RUFNUMMER, RUFNUMMER_INPUT)
+
+        # PASSWORT EINGEBEN
+        page.click(PASSWORT_INPUT)
+        page.fill(PASSWORT, PASSWORT_INPUT)
+
+        # BUCHUNGSÜBERSICHT ÖFFNEN
+        page.goto(UEBERSICHT_URL, wait_until="domcontentloaded")
+    except PlaywrightError as e:
+        if "Executable doesn't exist" in str(e):
+            print(
+                "Playwright konnte nicht starten. Hast du schon 'playwright install' in diesem Verzeichnis ausgeführt?"
+            )
+        else:
+            print(f"Playwright Error: {e}")
